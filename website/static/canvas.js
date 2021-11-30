b333b4c10ec31abf1302909d198bbc97d7318b8e
KevinL10 <kevinsliu0@gmail.com>
2021-11-29 23:08:15 -0800

    add button to clear canvas

--- website/static/canvas.js
+++ website/static/canvas.js
@@ -1,36 +1,41 @@
-canvas = document.getElementById("canvas");
-ctx = canvas.getContext("2d");
-width = canvas.width;
-height = canvas.height;
+window.addEventListener("load", () => {
+	var canvas = document.getElementById("canvas");
+	var ctx = canvas.getContext("2d");
+	var width = canvas.width;
+	var height = canvas.height;
+	var isDrawing = false;
 
-var isDrawing = false;
+	function getMousePos(canvas, evt) {
+	    var rect = canvas.getBoundingClientRect();
+	    return {
+	        x: (evt.clientX - rect.left) / (rect.right - rect.left) * width,
+	        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * height
+	    };
+	}
 
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
-        y: (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
-    };
-}
+	function draw(e){
+		if (!isDrawing) return;
 
-function draw(e){
-	if (!isDrawing) return;
+		var pos = getMousePos(canvas, e);
+		ctx.lineTo(pos.x, pos.y);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.moveTo(pos.x, pos.y);
+	}
 
-	var pos = getMousePos(canvas, e);
-	ctx.lineTo(pos.x, pos.y);
-	ctx.stroke();
-	ctx.beginPath();
-	ctx.moveTo(pos.x, pos.y);
-}
-
-canvas.addEventListener("mousedown", function(){
-	isDrawing = true;
-	ctx.beginPath();
+	canvas.addEventListener("mousedown", function(){
+		isDrawing = true;
+		ctx.beginPath();
+	});
+	canvas.addEventListener("mouseup", function(){
+		isDrawing = false;
+	});
+	canvas.addEventListener("mouseout", function(){
+		isDrawing = false;
+	});
+	canvas.addEventListener("mousemove", draw);
+	document.getElementById('clearCanvas').onclick = function(){
+		ctx.clearRect(0, 0, width, height);
+	};
 });
-canvas.addEventListener("mouseup", function(){
-	isDrawing = false;
-});
-canvas.addEventListener("mouseout", function(){
-	isDrawing = false;
-});
-canvas.addEventListener("mousemove", draw);
+
