7e738b69142a4981719c0ecf0404184299e359bc
KevinL10 <kevinsliu0@gmail.com>
2022-01-08 23:20:05 -0800

    clean up app and add both images

--- website/app.py
+++ website/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory, session
-from werkzeug.utils import secure_filename
 from scripts.bezier.derivify import derivify_bezier
 from scripts.leastsquares.derivify import derivify_least_squares
 import os, shutil
@@ -20,24 +19,35 @@ def index():
     # Give user a session id for their first visit
     if 'user_id' not in session:
         session['user_id'] = os.urandom(16).hex()
+        return render_template('index.html')
+
+    # If the user has previous graphs, display those
+    user_graphs_path = os.path.join(app.config['UPLOAD_PATH'], session['user_id'])
+    if os.path.exists(user_graphs_path):
+        return render_template('index.html', files=os.listdir(user_graphs_path))
 
     return render_template('index.html')
 
 @app.route('/derivify', methods = ['POST'])
 def derivify():
-    input_file = request.files['file']
-    filename = secure_filename(input_file.filename)
-    user_path = os.path.join(app.config['UPLOAD_PATH'], session['user_id'])
+    user_image = request.files['file']
+    user_graphs_path = os.path.join(app.config['UPLOAD_PATH'], session['user_id'])
 
-    if filename != '':
+    if user_image.filename != '':
         # Remove the user's previous directory and create a new one
-        shutil.rmtree(user_path, ignore_errors=True)
-        os.mkdir(user_path)
+        shutil.rmtree(user_graphs_path, ignore_errors=True)
+        os.mkdir(user_graphs_path)
 
-        # Files named upload.bmp and deriv.png
-        new_path = os.path.join(user_path, 'upload.bmp')
-        input_file.save(new_path)
-        derivify_least_squares(new_path, os.path.join(user_path, 'deriv.png'))
+        user_image_path = os.path.join(user_graphs_path, 'user_image.png')
+        user_image.save(user_image_path)
+
+        # If any errors occur, delete the temporary files within the user's directory
+        try:
+            derivify_least_squares(user_image_path, os.path.join(user_graphs_path, 'deriv_least_squares.png'))
+            derivify_bezier(user_image_path, os.path.join(user_graphs_path, 'deriv_bezier.png'))
+        except Exception as e:
+            print(f"[+] Error occured {e}")
+            shutil.rmtree(user_graphs_path, ignore_errors=True)
 
     return redirect(url_for('index'))
 
